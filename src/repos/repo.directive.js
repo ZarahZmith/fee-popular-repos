14cbe758398e18f918ff54e48b5db90824e2eca7
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-10 14:39:53 -0400

    Created a preliminary directive for the repo

---
+++ src/repos/repo.directive.js
@@ -0,0 +1,13 @@
+(function() {
+    'use strict';
+
+    angular.module('gh')
+        .directive('repo', repo);
+
+    function repo() {
+        return {
+            templateUrl: 'repos/repo.template.html',
+            restrict: 'E'
+        };
+    }
+}());
