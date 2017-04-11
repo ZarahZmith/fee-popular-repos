c0dcc72f7369ab9da00ef1b6bf9f7848c0b55c48
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-11 16:24:23 -0400

    Created a copy of the array to avoid getting trapped in an infinate loop. Sucessfully created a function to sort by popularity

--- src/repos/popularity.filter.js
+++ src/repos/popularity.filter.js
@@ -12,6 +12,14 @@
                 return repo;
             }
 
+            let repoC = [].concat(repo);
+
+            return repoC.sort(function doPopularitySorting(a, b) {
+                let aPop = a.stargazers_count + (a.forks_count*2) + (a.open_issues_count/2);
+                let bPop = b.stargazers_count + (b.forks_count*2) + (b.open_issues_count/2);
+
+                return bPop - aPop;
+            });
         };
     }
 }());
