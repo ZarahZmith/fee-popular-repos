000bd4bedde4073240f082975daab87f84f48990
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-11 16:31:02 -0400

    Removed unneccesary line from code

--- src/repos/popularity.filter.js
+++ src/repos/popularity.filter.js
@@ -5,8 +5,6 @@
         .filter('popularity', popularity);
 
     function popularity() {
-        let $ = angular.element;
-
         return function arrangePopularity(repo) {
             if (!Array.isArray(repo)) {
                 return repo;
